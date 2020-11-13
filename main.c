7b777af653f963b7e55f079edeb347a3882c933c
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-13 15:28:50 +0000

    what happens in shell so far

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MAX_WORD 10
+#define MAX_CHAR 100
+
+
+
+void remove_end0fLine(char line[])
+{
+  int i = 0;
+  while(line[i] != '\n')
+    i++;
+  line[i] = '\0';
+}
+
+
+void read_line(char line[])
+{
+  fgets(line,MAX_CHAR,stdin);
+  remove_end0fLine(line);
+
+}
+int process_line(char* args[], char line)
+{
+  int i= 0;
+  args[i] = strtok(line, " ");
+
+
+  if(args[i] == NULL)
+    {
+      return 1;
+    }
+
+
+  while(args[i] != NULL)
+    {
+      i++;
+      args[i]= strtok(NULL," ");
+    }
+  return 1;
+}
+int read_parse_line(char* args[], char line[])
+{
+  read_line(line);
+  process_line(args,line);
+  return 1;
+}
+int main ()
+{
+  char* args[MAX_WORD];
+  char line[MAX_CHAR];
+
+  while(read_parse_line(args,line))
+    {
+
+
+
+      pid_t pid =fork();
+
+      if (pid == 0)
+	{
+	  execvp(args[0],args);
+	}
+      else
+	{
+	  waitpid(pid, 0);
+	}
+    }
+  return 0;
+}
+
