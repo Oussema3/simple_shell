ee05b0192576a10771a7b9732d2c97686a4b4966
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-13 15:44:16 +0000

    where are we ?

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define MAX_WORD 10
 #define MAX_CHAR 100
 
@@ -11,18 +14,25 @@ void remove_end0fLine(char line[])
 {
   int i = 0;
   while(line[i] != '\n')
-    i++;
-  line[i] = '\0';
+    {
+
+
+      i++;
+      line[i] = '\0';
+    }
 }
 
 
 void read_line(char line[])
 {
-  fgets(line,MAX_CHAR,stdin);
+  char* ret = fgets(line,MAX_CHAR,stdin);
+
   remove_end0fLine(line);
+  if( strcmp(line,"exit")== 0 || ret == NULL)
+    exit(0);
 
 }
-int process_line(char* args[], char line)
+int process_line(char* args[], char line[])
 {
   int i= 0;
   args[i] = strtok(line, " ");
@@ -30,6 +40,7 @@ int process_line(char* args[], char line)
 
   if(args[i] == NULL)
     {
+      printf("NO COMMAND\n");
       return 1;
     }
 
@@ -43,6 +54,7 @@ int process_line(char* args[], char line)
 }
 int read_parse_line(char* args[], char line[])
 {
+  int i = 0;
   read_line(line);
   process_line(args,line);
   return 1;
@@ -57,7 +69,7 @@ int main ()
 
 
 
-      pid_t pid =fork();
+      pid_t pid = fork();
 
       if (pid == 0)
 	{
@@ -65,7 +77,7 @@ int main ()
 	}
       else
 	{
-	  waitpid(pid, 0);
+	  waitpid(pid,0);
 	}
     }
   return 0;
