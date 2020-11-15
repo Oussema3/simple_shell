d6fddce7618687eea5fd6303edc428d4d0cdbc74
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-15 05:36:47 +0000

    where are we now ?

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 #define MAX_WORD 10
 #define MAX_CHAR 100
 
+int input_redirection_flag ;
+int output_redirection_flag ;
+int piping_flag;
+char* input_file = NULL;
+char* output_file = NULL;
+
 //plz get added now FFS
 //adding comments to check commiting
 //Readding comments to recheck commiting
@@ -45,26 +51,17 @@ void read_line(char line[])
 
 
 
-int process_line(char* args[], char line[])
+int process_line(char* temp[], char line[])
 {
-
+  
   int i= 0;
-  args[i] = strtok(line, " ");
+  temp[i] = strtok(line, " ");
 
-  if(args[i] == NULL)
-    {
-
-      printf("NO COMMAND\n");
-      return 1;
-
-    }
-
-
-  while(args[i] != NULL)
+  while(temp[i] != NULL)
     {
 
       i++;
-      args[i]= strtok(NULL," ");
+      temp[i]= strtok(NULL," ");
 
     }
 
@@ -73,25 +70,112 @@ int process_line(char* args[], char line[])
 }
 
 
+int pipe_ad_redirection_checking(char* temp[])
 
+{
+  int i = 0;
+  while(temp[i] != NULL)
+    {
+      if(strcmp(temp[i],">") == 0)
+	{
+	  output_redirection_flag = 1; 
+	  output_file = temp[i+1];
+	  return i;
+	}
+      if(strcmp(temp[i],"<") == 0)
+	{
+	  input_redirection_flag = 1;
+	  input_file = temp[i+1];
+	  return i;
+	}
+      if(strcmp(temp[i],"|") == 0)
+	{
+	  piping_flag = 1;
+	  return i;
+	}
+      i++;
+    }
+  return i;
+}
 
+void check_line(char* temp[])
 
-int read_parse_line(char* args[], char line[])
 {
 
- int i = 0;
+  int i = 0;
+  int pipe_cnt = 0;
+  int output_redirection_cnt = 0;
+  int input_redirection_cnt = 0;
+  int total_cnt= 0;
+
+  if(temp[i] == NULL)
+    {
+
+      printf("NO COMMAND\n");
+      return ;
+    }
+
+
+  while(temp[i] != NULL)
+    {
+      if(strcmp(temp[i],">")==0)
+	output_redirection_cnt++;
+
+      if(strcmp(temp[i],"<")==0)
+	input_redirection_cnt++;
+
+      if(strcmp(temp[i],"|")==0)
+	pipe_cnt++;
+
+      i++;
+    }
+  total_cnt = input_redirection_cnt+output_redirection_cnt+pipe_cnt;
+  if(total_cnt > 1)
+    {
+    printf("ERR: OUR SHELL DOES NOT HANDLE THAT CASE\n");
+    temp[0] = NULL;
+    }
+}
+
+int read_parse_line(char* args[], char line[],char* piping_args[])
+{
+  char* temp[MAX_WORD];
+  int i = 0;
+  int pos ;
   read_line(line);
-  process_line(args,line);
+  process_line(temp,line);
+
+  check_line(temp);
+
+  pos = pipe_ad_redirection_checking(temp);
+  while(i < pos)
+    {
+      args[i] = temp[i];
+      i++;
+    }
+  args[i] = NULL;
+  i++;
+  if(piping_flag == 1)
+    {
+      int j = 0;
+      while(temp[i] != NULL)
+	{
+	  piping_args[j] = temp[i];
+	  i++;
+	  j++;
+	}
+    }
   return 1;
 
  }
 int main ()
 {
-
+  
   char* args[MAX_WORD];
+char* piping_args[MAX_WORD]
   char line[MAX_CHAR];
 
-  while(read_parse_line(args,line))
+ while(read_parse_line(args,line,piping_args))
     {
 
 
@@ -99,15 +183,26 @@ int main ()
 
       if (pid == 0)
 	{
+	  if(input_redirection_flag == 1 && input_file != NULL)
+	    dup2(open(input_file,O_RDWR|O_CREAT,0777),0);
+
+	  if(output_redirection_flag == 1 && output_file != NULL)
+	    dup2(open(output_file,O_RDWR|O_CREAT,0777),1);
+
+	  if(piping_flag == 1);
 
 	  execvp(args[0],args);
 
 	}
       else
 	{
-
+	  
 	  waitpid(pid,0,0);
-
+	  input_redirection_flag = 0;
+	  output_redirection_flag = 0;
+	  piping_flag = 0;
+	  input_file == NULL;
+	  output_file == NULL;
 	}
     }
 
