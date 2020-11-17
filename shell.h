a1d4e2f61290e000bf7fd57df4ec826df8c81f20
chedly99 <2276@holbertonschool.com>
2020-11-17 22:50:34 +0100

    HEADer

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+// libraries //
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MAX_WORD 10
+#define MAX_CHAR 100
+
+// functions //
+
+
+void remove_end0fLine(char line[]);
+void read_line(char line[]);
+int process_line(char* temp[], char line[]);
+int pipe_ad_redirection_checking(char* temp[]);
+void check_line(char* temp[]);
+int read_parse_line(char* args[], char line[],char* piping_args[]);
+
+#endif
