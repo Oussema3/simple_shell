9e5e139cbe5f3fd3834318aa42a4d6c89ec2d56a
chedly99 <>
2020-11-24 18:13:39 +0100

    fix

--- main.c
+++ main.c
@@ -18,18 +18,6 @@ char* output_file = NULL;
 
 
 
-int process_line(char* temp[], char line[])
-{
-  int i= 0;
-
-  temp[i] = strtok(line, " ");
-  while(temp[i] != NULL)
-    {
-      i++;
-      temp[i]= strtok(NULL," ");
-    }
-  return 1;
-}
 
 int pipe_ad_redirection_checking(char* temp[])
 {
@@ -59,38 +47,6 @@ int pipe_ad_redirection_checking(char* temp[])
   return i;
 }
 
-void check_line(char* temp[])
-{
-  int i = 0;
-  int pipe_cnt = 0;
-  int output_redirection_cnt = 0;
-  int input_redirection_cnt = 0;
-  int total_cnt= 0;
-
-  if(temp[i] == NULL)
-    {
-      printf("NO COMMAND\n");
-      return ;
-    }
-  while(temp[i] != NULL)
-    {
-      if(strcmp(temp[i],">")==0)
-	output_redirection_cnt++;
-
-      if(strcmp(temp[i],"<")==0)
-	input_redirection_cnt++;
-
-      if(strcmp(temp[i],"|")==0)
-	pipe_cnt++;
-      i++;
-    }
-  total_cnt = input_redirection_cnt+output_redirection_cnt+pipe_cnt;
-  if(total_cnt > 1)
-    {
-    printf("ERR: OUR SHELL DOES NOT HANDLE THAT CASE\n");
-    temp[0] = NULL;
-    }
-}
 
 int read_parse_line(char* args[], char line[],char* piping_args[])
 {
