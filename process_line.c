5f6d22cf9aba613a9d9bba5daae4f3b0cc213f3e
chedly99 <>
2020-11-24 17:47:54 +0100

    procces line

---
+++ process_line.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+*process_line - split the command
+*@temp: character
+*@line: line to be procced
+*Return: integer
+*/
+
+int process_line(char *temp[], char line[])
+{
+int i = 0;
+
+temp[i] = strtok(line, " ");
+while (temp[i] != NULL)
+{
+i++;
+temp[i] = strtok(NULL, " ");
+}
+return (1);
+}
