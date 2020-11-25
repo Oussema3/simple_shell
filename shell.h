3d8fb03ce808b4c3bf333971ceca185c4b008202
chedly99 <>
2020-11-25 14:39:18 +0100

    HEAdeR

--- shell.h
+++ shell.h
@@ -15,16 +15,13 @@
 #define MAX_WORD 10
 #define MAX_CHAR 100
 
-int input_redirection_flag ;
-int output_redirection_flag ;
-int piping_flag;
 
 
 void remove_end0fLine(char line[]);
 int read_line(char line[]);
-int process_line(char* temp[], char line[]);
-int pipe_ad_redirection_checking(char* temp[]);
-void check_line(char* temp[]);
-int read_parse_line(char* args[], char line[],char* piping_args[]);
+int process_line(char *temp[], char line[]);
+int pipe_ad_redirection_checking(char *temp[]);
+void check_line(char *temp[]);
+int read_parse_line(char *args[], char line[], char *piping_args[]);
 
 #endif
